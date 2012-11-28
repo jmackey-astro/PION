8b71436583a622f556e999409ff006985c827db6
Jonathan Mackey <jmackey@bitbucket.org>
2012-11-28 09:28:46 +0100

    minor changes

--- extra_libraries/install_liblinks.sh
+++ extra_libraries/install_liblinks.sh
@@ -9,7 +9,7 @@
 #./install_silo.sh
 
 
-ln -s ../../../trunk/extra_libraries/lib ./lib
-ln -s ../../../trunk/extra_libraries/include ./include
-ln -s ../../../trunk/extra_libraries/bin ./bin
+ln -s ../../uniform_grid_code/trunk/extra_libraries/lib ./lib
+ln -s ../../uniform_grid_code/trunk/extra_libraries/include ./include
+ln -s ../../uniform_grid_code/trunk/extra_libraries/bin ./bin
 
