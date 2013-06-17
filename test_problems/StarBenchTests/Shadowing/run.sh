368503ff82684b2cd8dfd429c3c27222fcc8d55f
Jonathan Mackey <jmackey@bitbucket.org>
2013-06-17 14:12:24 +0200

    working on shadowing test.

--- test_problems/StarBenchTests/Shadowing/run.sh
+++ test_problems/StarBenchTests/Shadowing/run.sh
@@ -4,14 +4,8 @@
 #data_dir=/Users/jmackey/data/StarBench
 data_dir=/vol/aibn129/aibn129_1/jmackey/scratch/StarBench
 movie_dir=.
+rm ${movie_dir}/*.mp4
 
-ffmpeg -f image2 -r 10.0 \
- -i ${data_dir}/fig_TremblinMixing_D2_N064_nH0p5_Native_%03d.png \
- -sameq -s 938x768 ${movie_dir}/TremblinMixing_D2_N064_nH0p5_Native.mp4
-ffmpeg -f image2 -r 10.0 \
- -i ${data_dir}/fig_TremblinMixing_D2_N064_nH5_Native_%03d.png \
- -sameq -s 938x768 ${movie_dir}/TremblinMixing_D2_N064_nH5_Native.mp4
-exit
 
 #../../../icgen_serial params_TremblinMixing_D2_N064.txt silo
 #../../../icgen_serial params_TremblinMixing_D2_N064_nH5.txt silo
