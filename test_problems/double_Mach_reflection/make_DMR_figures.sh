7c51608dd6338acd5f18dc70112249b26e46465b
jmackey macbook2015 <mackey@ph1.uni-koeln.de>
2016-03-03 10:25:04 +0100

    Added new DMR files, and worked on silo2ascii (but it needs working parallel grid...).

--- test_problems/double_Mach_reflection/make_DMR_figures.sh
+++ test_problems/double_Mach_reflection/make_DMR_figures.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
 
-# call with ./make_DMR_figures.sh $test_dir $code_dir $data_dir
+# call with ./make_DMR_figures.sh $test_dir $code_dir $data_dir $visit_cmd $resolution
 test_dir=$1
 code_dir=$2
 data_dir=$3
 visit_cmd=$4
+RESOLUTION=$5
 
-echo "GENERATING PLOTS"
-cd ${test_dir}/test_DMR/
+echo "GENERATING PLOTS for resolution ${RESOLUTION}"
+cd ${test_dir}
 
-#./plot_DMR_results.sh ${data_dir} DMRm10t60_n260_EINT_av10
-#cd ${data_dir}
-## lo-res
-#convert -crop 955x325+175+225 DMRm10t60_n260_EINT_av10.tif DMRm10t60_n260_EINT_av10.tif 
-#convert DMRm10t60_n260_EINT_av10.tif -pointsize 20 -annotate +250+275 \
-# 'Nx=260, EINT/ETOT solver (RPV=3), A.V. eta=0.1' DMRm10t60_n260_EINT_av10.tif
-#exit
+./plot_DMR_results.sh ${data_dir} DMRm10t60_n${RESOLUTION}_Hyb_av10 $visit_cmd
+sleep 10
+./plot_DMR_results.sh ${data_dir} DMRm10t60_n${RESOLUTION}_RCV_av10 $visit_cmd
+sleep 10
+./plot_DMR_results.sh ${data_dir} DMRm10t60_n${RESOLUTION}_RCV_Hcor $visit_cmd
+sleep 10
+./plot_DMR_results.sh ${data_dir} DMRm10t60_n${RESOLUTION}_RPV_av10 $visit_cmd
+sleep 10
+./plot_DMR_results.sh ${data_dir} DMRm10t60_n${RESOLUTION}_FVS_av10 $visit_cmd
+sleep 10
+
+
+##########  FOR TESTING  ##########
+#
+# Crop figures down to the bare minimum of white space.
+#
+cd ${data_dir}
+# lo-res
+convert -crop 955x325+175+225 DMRm10t60_n${RESOLUTION}_Hyb_av10.png DMRm10t60_n${RESOLUTION}_Hyb_av10.png 
+convert -crop 955x325+175+225 DMRm10t60_n${RESOLUTION}_RCV_av10.png DMRm10t60_n${RESOLUTION}_RCV_av10.png 
+convert -crop 955x325+175+225 DMRm10t60_n${RESOLUTION}_RCV_Hcor.png DMRm10t60_n${RESOLUTION}_RCV_Hcor.png 
+convert -crop 955x325+175+225 DMRm10t60_n${RESOLUTION}_RPV_av10.png DMRm10t60_n${RESOLUTION}_RPV_av10.png 
+convert -crop 955x325+175+225 DMRm10t60_n${RESOLUTION}_FVS_av10.png DMRm10t60_n${RESOLUTION}_FVS_av10.png 
+#
+# Now annotate each figure with its attributes
+#
+convert DMRm10t60_n${RESOLUTION}_Hyb_av10.png -pointsize 20 -annotate +250+275 \
+ 'Nx=${RESOLUTION}, Primitive Var. Riemann solver, A.V. eta=0.1' DMRm10t60_n${RESOLUTION}_Hyb_av10.png
+convert DMRm10t60_n${RESOLUTION}_RCV_av10.png -pointsize 20 -annotate +250+275 \
+ 'Nx=${RESOLUTION}, Roe Conserved Var. flux solver, A.V. eta=0.1' DMRm10t60_n${RESOLUTION}_RCV_av10.png
+convert DMRm10t60_n${RESOLUTION}_RCV_Hcor.png -pointsize 20 -annotate +250+275 \
+ 'Nx=${RESOLUTION}, Roe Conserved Var. flux solver, H-correction' DMRm10t60_n${RESOLUTION}_RCV_Hcor.png
+convert DMRm10t60_n${RESOLUTION}_RPV_av10.png -pointsize 20 -annotate +250+275 \
+ 'Nx=${RESOLUTION}, Roe Primitive Var. flux solver, A.V. eta=0.1' DMRm10t60_n${RESOLUTION}_RPV_av10.png
+convert DMRm10t60_n${RESOLUTION}_FVS_av10.png -pointsize 20 -annotate +250+275 \
+ 'Nx=${RESOLUTION}, Flux Vector Splitting   solver, A.V. eta=0.1' DMRm10t60_n${RESOLUTION}_FVS_av10.png
+
+
+exit
+##########  FOR TESTING  ##########
 
 ./plot_DMR_results.sh ${data_dir} DMRm10t60_n260_Hyb_av00 $visit_cmd
 sleep 10
--- test_problems/double_Mach_reflection/run_DMR_tests.sh
+++ test_problems/double_Mach_reflection/run_DMR_tests.sh
@@ -56,14 +56,24 @@ fi
 # Double Mach Reflection, same as Stone et al. 2008.
 #
 echo "DOUBLE MACH REFLECTION: GENERATE ICS"
-./icgen_serial ${test_dir}/params_DMR_n260.txt silo redirect=${data_dir}/ic_n260_
-./icgen_serial ${test_dir}/params_DMR_n520.txt silo redirect=${data_dir}/ic_n520_
+./icgen_serial ${test_dir}/params_DMR_n130.txt silo redirect=${data_dir}/ic_n260_
+#./icgen_serial ${test_dir}/params_DMR_n260.txt silo redirect=${data_dir}/ic_n260_
+#./icgen_serial ${test_dir}/params_DMR_n520.txt silo redirect=${data_dir}/ic_n520_
 
-#./main_serial IC_DMRm10t60_n260.silo 5 1 outfile=${data_dir}/DMRm10t60_n260_EINT_av10 cfl=0.5 \
-# redirect=${data_dir}/msg_DMRm10t60_n260_EINT_av10 solver=3 eqntype=9 AVtype=1 EtaVisc=0.1
-#exit
-
-
+echo "DOUBLE MACH REFLECTION: RUN LOW RES"
+./pion_serial IC_DMRm10t60_n130.silo 5 1 outfile=${data_dir}/DMRm10t60_n130_Hyb_av10 cfl=0.4 artvisc=0.1 \
+ redirect=${data_dir}/msg_DMRm10t60_n130_Hyb_av10 solver=3 &
+./pion_serial IC_DMRm10t60_n130.silo 5 1 outfile=${data_dir}/DMRm10t60_n130_RCV_av10 cfl=0.4 artvisc=0.1 \
+ redirect=${data_dir}/msg_DMRm10t60_n130_RCV_av10 solver=4 &
+./pion_serial IC_DMRm10t60_n130.silo 5 1 outfile=${data_dir}/DMRm10t60_n130_RPV_av10 cfl=0.4 artvisc=0.1 \
+ redirect=${data_dir}/msg_DMRm10t60_n130_RPV_av10 solver=5 &
+wait
+./pion_serial IC_DMRm10t60_n130.silo 5 1 outfile=${data_dir}/DMRm10t60_n130_FVS_av10 cfl=0.4 artvisc=0.1 \
+ redirect=${data_dir}/msg_DMRm10t60_n130_FVS_av10 solver=6 &
+./pion_serial IC_DMRm10t60_n130.silo 5 1 outfile=${data_dir}/DMRm10t60_n130_RCV_Hcor cfl=0.4 AVtype=3    \
+ redirect=${data_dir}/msg_DMRm10t60_n130_RCV_Hcor solver=4 &
+wait
+exit
 
 #
 echo "DOUBLE MACH REFLECTION: RUN LOW RES"
