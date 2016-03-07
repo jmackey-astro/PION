fd64fdf91630a8416f16791f1b06003d23c6be08
jmackey macbook2015 <mackey@ph1.uni-koeln.de>
2016-03-07 01:29:41 +0000

    Added more FieldLoop test files, and modified scripts so that the test works now (at least on os-x).

--- test_problems/FieldLoop/magp_decay.sh
+++ test_problems/FieldLoop/magp_decay.sh
@@ -5,60 +5,58 @@
 #
 # -JM 2009-12-16 File created.  currently not run by default for the test problems.
 # -JM 2010.12.06 Changed so that it reads the test runs, and runs by default.
+# - 2016.03.06 JM: updated to work for different resolutions.
 #
 
-#infile=./FL_LimVA_oa2av10_Lin_info.txt
 input_dir=$1
 outfile=$2
+resolution=$3
 
 cat << EOF  > gnu.plt_VA_FKJ
 set terminal postscript enhanced color eps
-set output "${outfile}_VA_FKJ.eps"
+set output "${input_dir}/${outfile}_VA_FKJ.eps"
 set size 0.5, 0.5
 set xlabel "Time"
 set ylabel "Magnetic Pressure" offset 0.8,0.0
 set xrange [0:2]
-set yrange [0.8:1.0]
+#set yrange [0.8:1.0]
 set key top right
-plot '${input_dir}/msg_FieldLoop200_Lin_static_av1info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Lin_planar_av1info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Lin_VxVyVz_av1info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Lin_VxVyVz_av0info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
+plot '${input_dir}/msg_FieldLoop${resolution}_Lin_static_av1info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Lin_planar_av1info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Lin_VxVyVz_av1info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Lin_VxVyVz_av0info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
 EOF
+
 
 cat << EOF  > gnu.plt_VA_ROE
 set terminal postscript enhanced color eps
-set output "${outfile}_VA_ROE.eps"
+set output "${input_dir}/${outfile}_VA_ROE.eps"
 set size 0.5, 0.5
 set xlabel "Time"
 set ylabel "Magnetic Pressure" offset 0.8,0.0
 set xrange [0:2]
-set yrange [0.8:1.0]
+#set yrange [0.8:1.0]
 set key top right
-plot '${input_dir}/msg_FieldLoop200_Roe_static_av1info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_planar_av1info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_VxVyVz_av1info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_VxVyVz_av0info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
+plot '${input_dir}/msg_FieldLoop${resolution}_Roe_static_av1info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_planar_av1info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_av1info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_av0info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
 EOF
 
-#
-# Once I code the H-correction for MHD I can also make this plot for
-# H-corrected fluxes.  Although I doubt it will make much difference
-# since the main effect seems to come from the slope limiting.
-#
+
 cat << EOF  > gnu.plt_VA_ROE_HCORR
 set terminal postscript enhanced color eps
-set output "${outfile}_VA_ROE_Hcorr.eps"
+set output "${input_dir}/${outfile}_VA_ROE_Hcorr.eps"
 set size 0.5, 0.5
 set xlabel "Time"
-set ylabel "Magnetic Pressure" 0.8
+set ylabel "Magnetic Pressure" offset 0.8,0.0
 set xrange [0:2]
-set yrange [0.8:1.0]
+#set yrange [0.8:1.0]
 set key top right
-plot '${input_dir}/msg_FieldLoop200_Roe_static_Hcorr_info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_planar_Hcorr_info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_VxVyVz_Hcorr_info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
-     '${input_dir}/msg_FieldLoop200_Roe_VxVyVz_av0_info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
+plot '${input_dir}/msg_FieldLoop${resolution}_Roe_static_Hcorr_info.txt' u 1:2 w l title 'Static {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_planar_Hcorr_info.txt' u 1:2 w l title 'Planar {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_Hcorr_info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.10', \
+     '${input_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_av0_info.txt' u 1:2 w l title 'Vel.3D {/Symbol h}=0.00'
 EOF
 
 
@@ -66,81 +64,11 @@ gnuplot gnu.plt_VA_FKJ
 gnuplot gnu.plt_VA_ROE
 gnuplot gnu.plt_VA_ROE_HCORR
 
-convert -density 300 -quality 100 ${outfile}_VA_FKJ.eps ${outfile}_VA_FKJ.jpeg
-convert -density 300 -quality 100 ${outfile}_VA_ROE.eps ${outfile}_VA_ROE.jpeg
-convert -density 300 -quality 100 ${outfile}_VA_ROE_HCORR.eps ${outfile}_VA_ROE_HCORR.jpeg
+convert -density 300 -quality 100 ${input_dir}/${outfile}_VA_FKJ.eps ${input_dir}/${outfile}_VA_FKJ.png
+convert -density 300 -quality 100 ${input_dir}/${outfile}_VA_ROE.eps ${input_dir}/${outfile}_VA_ROE.png
+convert -density 300 -quality 100 ${input_dir}/${outfile}_VA_ROE_HCORR.eps ${input_dir}/${outfile}_VA_ROE_HCORR.png
 
 
 exit
 
 
-cat << EOF  > gnu.plt_VA_FKJ
-set terminal postscript enhanced color eps
-set output "${outfile}_VA_FKJ.eps"
-set size 0.5, 0.5
-set xlabel "Time"
-set ylabel "Magnetic Pressure" 0.8
-set xrange [0:2]
-set yrange [0.8:1.0]
-set key top right
-plot './FL_LimVA_oa2av10_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.10', \
-     './FL_LimVA_oa2av01_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.01', \
-     './FL_LimVA_oa2av00_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.00'
-EOF
-
-cat << EOF  > gnu.plt_VA_ROE
-set terminal postscript enhanced color eps
-set output "${outfile}_VA_ROE.eps"
-set size 0.5, 0.5
-set xlabel "Time"
-set ylabel "Magnetic Pressure" 0.8
-set xrange [0:2]
-set yrange [0.8:1.0]
-set key top right
-plot './FL_LimVA_oa2av10_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.10', \
-     './FL_LimVA_oa2av01_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.01', \
-     './FL_LimVA_oa2av00_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.00'
-EOF
-
-cat << EOF  > gnu.plt_MM_FKJ
-set terminal postscript enhanced color eps
-set output "${outfile}_MM_FKJ.eps"
-set size 0.5, 0.5
-set xlabel "Time"
-set ylabel "Magnetic Pressure" 0.8
-set xrange [0:2]
-set yrange [0.65:1.0]
-set key top right
-plot './FL_LimMM_oa2av10_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.10', \
-     './FL_LimMM_oa2av01_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.01', \
-     './FL_LimMM_oa2av00_Lin_info.txt' u 1:2 w l title '{/Symbol h}=0.00'
-EOF
-
-cat << EOF  > gnu.plt_MM_ROE
-set terminal postscript enhanced color eps
-set output "${outfile}_MM_ROE.eps"
-set size 0.5, 0.5
-set xlabel "Time"
-set ylabel "Magnetic Pressure" 0.8
-set xrange [0:2]
-set yrange [0.65:1.0]
-set key top right
-plot './FL_LimMM_oa2av10_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.10', \
-     './FL_LimMM_oa2av01_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.01', \
-     './FL_LimMM_oa2av00_Roe_info.txt' u 1:2 w l title '{/Symbol h}=0.00'
-EOF
-
-gnuplot gnu.plt_VA_FKJ
-gnuplot gnu.plt_VA_ROE
-#gnuplot gnu.plt_MM_FKJ
-#gnuplot gnu.plt_MM_ROE
-
-convert -density 300 -quality 100 ${outfile}_VA_FKJ.eps ${outfile}_VA_FKJ.jpeg
-convert -density 300 -quality 100 ${outfile}_VA_ROE.eps ${outfile}_VA_ROE.jpeg
-#convert -density 300 -quality 100 ${outfile}_MM_FKJ.eps ${outfile}_MM_FKJ.jpeg
-#convert -density 300 -quality 100 ${outfile}_MM_ROE.eps ${outfile}_MM_ROE.jpeg
-
-#cp FieldLoop_MM_FKJ.eps FieldLoop_VA_FKJ.eps   ~/active/jmthesis/trunk/figs/
-#cp FieldLoop_MM_FKJ.jpeg FieldLoop_VA_FKJ.jpeg ~/active/jmthesis/trunk/figs/
-
-exit
--- test_problems/FieldLoop/run_FL_test.sh
+++ test_problems/FieldLoop/run_FL_test.sh
@@ -4,6 +4,7 @@
 #
 # -JM 2009-12-15 Added appending of figs.
 # -JM 2009-12-17 Changed directories to be read in from args.
+# - 2016.03.06 JM: updated to work better...
 #
 
 
@@ -12,111 +13,97 @@ test_dir=${1}
 code_dir=${2}
 data_dir=${3}/FieldLoop
 visit_cmd=${4}
+resolution=${5}
 
 # Just in case it doesn't exist, create the destination directory.
 mkdir -p $data_dir
-#rm ${data_dir}/*
+
 
 cd ${code_dir}
-echo "MAKE IN" $code_dir
+echo "MAKE IN " $code_dir
 ./compile_code.sh
 echo "MAKE SUCEEDED"
 cd -
 
-../../icgen_serial ${test_dir}/params_FieldLoop.txt       silo redirect=tmp_
-../../icgen_serial ${test_dir}/params_FieldLoopVz.txt     silo redirect=tmp_
-../../icgen_serial ${test_dir}/params_FieldLoopStatic.txt silo redirect=tmp_
+../../icgen_serial ${test_dir}/params_FieldLoop${resolution}.txt       silo redirect=tmp_
+../../icgen_serial ${test_dir}/params_FieldLoop${resolution}vz.txt     silo redirect=tmp_
+../../icgen_serial ${test_dir}/params_FieldLoop${resolution}Static.txt silo redirect=tmp_
 
-../../pion_serial IC_FieldLoop200static.silo 5 1 outfile=${data_dir}/FieldLoop200static_Roe \
- redirect=${data_dir}/msg_FieldLoop200_Roe_static_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-../../pion_serial IC_FieldLoop200.silo 5 1 outfile=${data_dir}/FieldLoop200_Roe \
- redirect=${data_dir}/msg_FieldLoop200_Roe_planar_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-wait
-../../pion_serial IC_FieldLoop200vz.silo 5 1 outfile=${data_dir}/FieldLoop200vz_Roe \
- redirect=${data_dir}/msg_FieldLoop200_Roe_VxVyVz_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-../../pion_serial IC_FieldLoop200vz.silo 5 1 outfile=${data_dir}/FieldLoop200vz_RoeAV0 \
- redirect=${data_dir}/msg_FieldLoop200_Roe_VxVyVz_av0 cfl=0.4 AVtype=0 EtaVisc=0.0 solver=4 &
-wait
-
-../../pion_serial IC_FieldLoop200static.silo 5 1 outfile=${data_dir}/FieldLoop200static_Lin \
- redirect=${data_dir}/msg_FieldLoop200_Lin_static_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-../../pion_serial IC_FieldLoop200.silo 5 1 outfile=${data_dir}/FieldLoop200_Lin \
- redirect=${data_dir}/msg_FieldLoop200_Lin_planar_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-wait
-../../pion_serial IC_FieldLoop200vz.silo 5 1 outfile=${data_dir}/FieldLoop200vz_Lin \
- redirect=${data_dir}/msg_FieldLoop200_Lin_VxVyVz_av1 cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
-../../pion_serial IC_FieldLoop200vz.silo 5 1 outfile=${data_dir}/FieldLoop200vz_LinAV0 \
- redirect=${data_dir}/msg_FieldLoop200_Lin_VxVyVz_av0 cfl=0.4 AVtype=0 EtaVisc=0.0 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}static.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}static_Roe \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_static_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}_Roe \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_planar_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+#wait
+../../pion_serial IC_FieldLoop${resolution}vz.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}vz_Roe \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}vz.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}vz_RoeAV0 \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_av0_ cfl=0.4 AVtype=0 EtaVisc=0.0 solver=4 &
 wait
 
-../../pion_serial IC_FieldLoop200static.silo 5 1 outfile=${data_dir}/FieldLoop200static_Roe_Hcorr \
- redirect=${data_dir}/msg_FieldLoop200_Roe_static_Hcorr cfl=0.4 AVtype=3 solver=4 &
-../../pion_serial IC_FieldLoop200.silo 5 1 outfile=${data_dir}/FieldLoop200_Roe_Hcorr \
- redirect=${data_dir}/msg_FieldLoop200_Roe_planar_Hcorr cfl=0.4 AVtype=3 solver=4 &
-../../pion_serial IC_FieldLoop200vz.silo 5 1 outfile=${data_dir}/FieldLoop200vz_Roe_Hcorr \
- redirect=${data_dir}/msg_FieldLoop200_Roe_VxVyVz_Hcorr cfl=0.4 AVtype=3 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}static.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}static_Lin \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Lin_static_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}_Lin \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Lin_planar_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+#wait
+../../pion_serial IC_FieldLoop${resolution}vz.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}vz_Lin \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Lin_VxVyVz_av1_ cfl=0.4 AVtype=1 EtaVisc=0.1 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}vz.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}vz_LinAV0 \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Lin_VxVyVz_av0_ cfl=0.4 AVtype=0 EtaVisc=0.0 solver=4 &
 wait
 
-exit
+../../pion_serial IC_FieldLoop${resolution}static.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}static_Roe_Hcorr \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_static_Hcorr_ cfl=0.4 AVtype=3 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}_Roe_Hcorr \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_planar_Hcorr_ cfl=0.4 AVtype=3 solver=4 &
+../../pion_serial IC_FieldLoop${resolution}vz.silo 5 1 outfile=${data_dir}/FieldLoop${resolution}vz_Roe_Hcorr \
+ redirect=${data_dir}/msg_FieldLoop${resolution}_Roe_VxVyVz_Hcorr_ cfl=0.4 AVtype=3 solver=4 &
+wait
+
+
+#exit
+
 echo "Sims finished, moving to FieldLoop dir to make figures."
 cd ${test_dir}
-./make_FL_img.sh ${data_dir} FieldLoop200static_Roe FieldLoop200static_Roe $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200_Roe FieldLoop200_Roe             $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200vz_Roe FieldLoop200vz_Roe         $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200vz_RoeAV0 FieldLoop200vz_RoeAV0   $visit_cmd
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}static_Roe FieldLoop${resolution}static_Roe $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}_Roe FieldLoop${resolution}_Roe             $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}vz_Roe FieldLoop${resolution}vz_Roe         $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}vz_RoeAV0 FieldLoop${resolution}vz_RoeAV0   $visit_cmd
+#sleep 10
 
-./make_FL_img.sh ${data_dir} FieldLoop200static_Lin FieldLoop200static_Lin $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200_Lin FieldLoop200_Lin             $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200vz_Lin FieldLoop200vz_Lin         $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200vz_LinAV0 FieldLoop200vz_LinAV0   $visit_cmd
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}static_Lin FieldLoop${resolution}static_Lin $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}_Lin FieldLoop${resolution}_Lin             $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}vz_Lin FieldLoop${resolution}vz_Lin         $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}vz_LinAV0 FieldLoop${resolution}vz_LinAV0   $visit_cmd
+#sleep 10
 
-./make_FL_img.sh ${data_dir} FieldLoop200static_Roe_Hcorr FieldLoop200static_Roe_Hcorr $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200_Roe_Hcorr       FieldLoop200_Roe_Hcorr       $visit_cmd
-./make_FL_img.sh ${data_dir} FieldLoop200vz_Roe_Hcorr     FieldLoop200vz_Roe_Hcorr     $visit_cmd
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}static_Roe_Hcorr FieldLoop${resolution}static_Roe_Hcorr $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}_Roe_Hcorr       FieldLoop${resolution}_Roe_Hcorr       $visit_cmd
+#sleep 10
+./make_FL_img.sh ${data_dir} FieldLoop${resolution}vz_Roe_Hcorr     FieldLoop${resolution}vz_Roe_Hcorr     $visit_cmd
+#sleep 10
 
-convert ${data_dir}/FieldLoop200_Roe_MagP_00.tif ${data_dir}/FieldLoop200static_Roe_MagP_02.tif \
-    ${data_dir}/FieldLoop200_Roe_MagP_02.tif +append ${test_dir}/FL200_Roe_MagP.tif
-convert ${data_dir}/FieldLoop200_Lin_MagP_00.tif ${data_dir}/FieldLoop200static_Lin_MagP_02.tif \
-    ${data_dir}/FieldLoop200_Lin_MagP_02.tif +append ${test_dir}/FL200_Lin_MagP.tif
-convert ${data_dir}/FieldLoop200_Roe_Hcorr_MagP_00.tif ${data_dir}/FieldLoop200static_Roe_Hcorr_MagP_02.tif \
-    ${data_dir}/FieldLoop200_Roe_Hcorr_MagP_02.tif +append ${test_dir}/FL200_Roe_Hcorr_MagP.tif
+convert ${data_dir}/FieldLoop${resolution}_Roe_MagP_00.png ${data_dir}/FieldLoop${resolution}static_Roe_MagP_02.png \
+    ${data_dir}/FieldLoop${resolution}_Roe_MagP_02.png +append ${test_dir}/FL${resolution}_Roe_MagP.png
+convert ${data_dir}/FieldLoop${resolution}_Lin_MagP_00.png ${data_dir}/FieldLoop${resolution}static_Lin_MagP_02.png \
+    ${data_dir}/FieldLoop${resolution}_Lin_MagP_02.png +append ${test_dir}/FL${resolution}_Lin_MagP.png
+convert ${data_dir}/FieldLoop${resolution}_Roe_Hcorr_MagP_00.png ${data_dir}/FieldLoop${resolution}static_Roe_Hcorr_MagP_02.png \
+    ${data_dir}/FieldLoop${resolution}_Roe_Hcorr_MagP_02.png +append ${test_dir}/FL${resolution}_Roe_Hcorr_MagP.png
 
-convert ${data_dir}/FieldLoop200_Roe_CurlB2D_00.tif ${data_dir}/FieldLoop200static_Roe_CurlB2D_02.tif \
-    ${data_dir}/FieldLoop200_Roe_CurlB2D_02.tif +append ${test_dir}/FL200_Roe_CurlB2D.tif
-convert ${data_dir}/FieldLoop200_Lin_CurlB2D_00.tif ${data_dir}/FieldLoop200static_Lin_CurlB2D_02.tif \
-    ${data_dir}/FieldLoop200_Lin_CurlB2D_02.tif +append ${test_dir}/FL200_Lin_CurlB2D.tif
-convert ${data_dir}/FieldLoop200_Roe_Hcorr_CurlB2D_00.tif ${data_dir}/FieldLoop200static_Roe_Hcorr_CurlB2D_02.tif \
-    ${data_dir}/FieldLoop200_Roe_Hcorr_CurlB2D_02.tif +append ${test_dir}/FL200_Roe_Hcorr_CurlB2D.tif
+convert ${data_dir}/FieldLoop${resolution}_Roe_CurlB2D_00.png ${data_dir}/FieldLoop${resolution}static_Roe_CurlB2D_02.png \
+    ${data_dir}/FieldLoop${resolution}_Roe_CurlB2D_02.png +append ${test_dir}/FL${resolution}_Roe_CurlB2D.png
+convert ${data_dir}/FieldLoop${resolution}_Lin_CurlB2D_00.png ${data_dir}/FieldLoop${resolution}static_Lin_CurlB2D_02.png \
+    ${data_dir}/FieldLoop${resolution}_Lin_CurlB2D_02.png +append ${test_dir}/FL${resolution}_Lin_CurlB2D.png
+convert ${data_dir}/FieldLoop${resolution}_Roe_Hcorr_CurlB2D_00.png ${data_dir}/FieldLoop${resolution}static_Roe_Hcorr_CurlB2D_02.png \
+    ${data_dir}/FieldLoop${resolution}_Roe_Hcorr_CurlB2D_02.png +append ${test_dir}/FL${resolution}_Roe_Hcorr_CurlB2D.png
 
-convert ${test_dir}/FL200_Roe_MagP.tif    eps3:${test_dir}/FL200_Roe_MagP.eps
-convert ${test_dir}/FL200_Roe_CurlB2D.tif eps3:${test_dir}/FL200_Roe_CurlB2D.eps
-convert ${test_dir}/FL200_Lin_MagP.tif    eps3:${test_dir}/FL200_Lin_MagP.eps
-convert ${test_dir}/FL200_Lin_CurlB2D.tif eps3:${test_dir}/FL200_Lin_CurlB2D.eps
-convert ${test_dir}/FL200_Roe_Hcorr_MagP.tif    eps3:${test_dir}/FL200_Roe_Hcorr_MagP.eps
-convert ${test_dir}/FL200_Roe_Hcorr_CurlB2D.tif eps3:${test_dir}/FL200_Roe_Hcorr_CurlB2D.eps
 
-#convert ${test_dir}/FL200_Roe_MagP.tif ${test_dir}/FL200_Roe_MagP.png
-#convert ${test_dir}/FL200_Roe_MagP.png eps2:${test_dir}/FL200_Roe_MagP.eps
-#convert ${test_dir}/FL200_Roe_CurlB2D.tif ${test_dir}/FL200_Roe_CurlB2D.png
-#convert ${test_dir}/FL200_Roe_CurlB2D.png eps2:${test_dir}/FL200_Roe_CurlB2D.eps
-#convert ${test_dir}/FL200_Lin_MagP.tif ${test_dir}/FL200_Lin_MagP.png
-#convert ${test_dir}/FL200_Lin_MagP.png eps2:${test_dir}/FL200_Lin_MagP.eps
-#convert ${test_dir}/FL200_Lin_CurlB2D.tif ${test_dir}/FL200_Lin_CurlB2D.png
-#convert ${test_dir}/FL200_Lin_CurlB2D.png eps2:${test_dir}/FL200_Lin_CurlB2D.eps
-
-convert ${test_dir}/FL200_Roe_MagP.tif ${test_dir}/FL200_Roe_MagP.jpeg
-convert ${test_dir}/FL200_Roe_CurlB2D.tif ${test_dir}/FL200_Roe_CurlB2D.jpeg
-convert ${test_dir}/FL200_Lin_MagP.tif ${test_dir}/FL200_Lin_MagP.jpeg
-convert ${test_dir}/FL200_Lin_CurlB2D.tif ${test_dir}/FL200_Lin_CurlB2D.jpeg
-convert ${test_dir}/FL200_Roe_Hcorr_MagP.tif ${test_dir}/FL200_Roe_Hcorr_MagP.jpeg
-convert ${test_dir}/FL200_Roe_Hcorr_CurlB2D.tif ${test_dir}/FL200_Roe_Hcorr_CurlB2D.jpeg
-
-#DESTDIR=~/active/projects/MHD_ETs_2010/docs/rmhd_pillars/figs
-#cp ${test_dir}/FL200_Lin_MagP.jpeg    $DESTDIR/FieldLoop_MagP.jpeg
-#cp ${test_dir}/FL200_Lin_CurlB2D.jpeg $DESTDIR/FieldLoop_J.jpeg
-#cp ${test_dir}/FL200_Lin_MagP.eps    $DESTDIR/FieldLoop_MagP.eps
-#cp ${test_dir}/FL200_Lin_CurlB2D.eps $DESTDIR/FieldLoop_J.eps
-
-./magp_decay.sh ${data_dir} FieldLoop2D_n200
+./magp_decay.sh ${data_dir} FieldLoop2D_n${resolution}
 exit
 
