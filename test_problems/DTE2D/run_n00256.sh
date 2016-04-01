60933d0b4bd15c5710578bdc45f1d5fef66167c2
jmackey macbook2015 <mackey@ph1.uni-koeln.de>
2016-04-01 12:01:48 +0100

    Added Dtype expansion test (2D) to test problems.

---
+++ test_problems/DTE2D/run_n00256.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mpirun -np  1 ../../icgen_parallel params_DTE_D2_TTI_n00256.txt silo redirect=iclog
+mpirun -np  1 ../../pion_parallel IC_DTE_D2_TTI_n00256_0000.silo 5 1 outfile=DTE_D2_TTI_n00256_np01 redirect=logDTE_D2_TTI_n00256_np01 &
+
+sleep 5
+
+mpirun -np  2 ../../icgen_parallel params_DTE_D2_TTI_n00256.txt silo redirect=iclog
+mpirun -np  2 ../../pion_parallel IC_DTE_D2_TTI_n00256_0000.silo 5 1 outfile=DTE_D2_TTI_n00256_np02 redirect=logDTE_D2_TTI_n00256_np02 &
+
+sleep 5
+
+mpirun -np  4 ../../icgen_parallel params_DTE_D2_TTI_n00256.txt silo redirect=iclog
+mpirun -np  4 ../../pion_parallel IC_DTE_D2_TTI_n00256_0000.silo 5 1 outfile=DTE_D2_TTI_n00256_np04 redirect=logDTE_D2_TTI_n00256_np04 &
+
+wait
+
+mpirun -np  8 ../../icgen_parallel params_DTE_D2_TTI_n00256.txt silo redirect=iclog
+mpirun -np  8 ../../pion_parallel IC_DTE_D2_TTI_n00256_0000.silo 5 1 outfile=DTE_D2_TTI_n00256_np08 redirect=logDTE_D2_TTI_n00256_np08
+
+mpirun -np 16 ../../icgen_parallel params_DTE_D2_TTI_n00256.txt silo redirect=iclog
+mpirun -np 16 ../../pion_parallel IC_DTE_D2_TTI_n00256_0000.silo 5 1 outfile=DTE_D2_TTI_n00256_np16 redirect=logDTE_D2_TTI_n00256_np16
+
+cd ../../analysis/silocompare
+MAKE_UNAME=standard make clean
+MAKE_UNAME=standard make -j8
+cd -
+
+../../analysis/silocompare/silocompare . . DTE_D2_TTI_n00256_np01_0000.00 DOUBLE DTE_D2_TTI_n00256_np02_0000.00 DOUBLE cmp_DTE_D2_TTI_n00256_np02_np01np02 2
+../../analysis/silocompare/silocompare . . DTE_D2_TTI_n00256_np01_0000.00 DOUBLE DTE_D2_TTI_n00256_np04_0000.00 DOUBLE cmp_DTE_D2_TTI_n00256_np02_np01np04 2
+../../analysis/silocompare/silocompare . . DTE_D2_TTI_n00256_np01_0000.00 DOUBLE DTE_D2_TTI_n00256_np08_0000.00 DOUBLE cmp_DTE_D2_TTI_n00256_np02_np01np08 2
+../../analysis/silocompare/silocompare . . DTE_D2_TTI_n00256_np01_0000.00 DOUBLE DTE_D2_TTI_n00256_np16_0000.00 DOUBLE cmp_DTE_D2_TTI_n00256_np02_np01np16 2
+
+
