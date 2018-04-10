28d58efac08897400267c9c7264a4b8eb5d2a72f
jmackey macbook2016 <jmackey@cp.dias.ie>
2018-04-10 14:10:20 +0200

    updated project3D code and dataio to try to read legacy silo files.

--- analysis/Project3D_data/run_Bubble_test.sh
+++ analysis/Project3D_data/run_Bubble_test.sh
@@ -10,7 +10,7 @@ DATA_DIR=/Users/jm/data
 OP_DIR=/Users/jm/data
 mkdir -p $OP_DIR
 
-MAKE_UNAME=OSX make clean
+#MAKE_UNAME=OSX make clean
 MAKE_UNAME=OSX make -j8
 
 #
@@ -22,7 +22,7 @@ MAKE_UNAME=OSX make -j8
 
 SNAPSHOT=bubble3D_1b_medres_0000.0
 VTKFILE=proj3d_ZY_bubble3D_1b_medres
-mpirun -np 4 ./projection ${DATA_DIR} $SNAPSHOT ${OP_DIR}/${VTKFILE}_t15 3 1 ZN YP 25 7 0
+mpirun -np 1 ./projection ${DATA_DIR} $SNAPSHOT ${OP_DIR}/${VTKFILE}_t15 3 1 ZN YP 25 7 0
 #mpirun -np 8 ./projection ${DATA_DIR} $SNAPSHOT ${OP_DIR}/${VTKFILE}_t30 3 1 ZN YP 30 7 0
 #mpirun -np 8 ./projection ${DATA_DIR} $SNAPSHOT ${OP_DIR}/${VTKFILE}_t45 3 1 ZN YP 45 7 0
 #mpirun -np 8 ./projection ${DATA_DIR} $SNAPSHOT ${OP_DIR}/${VTKFILE}_t00 3 1 ZN YP 00 7 0
