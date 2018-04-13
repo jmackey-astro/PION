1ece6085c70900bb72188b851de5d4804db5bd50
Jonathan Mackey <jmackey@cp.dias.ie>
2018-04-13 17:53:26 +0100

    debugged HLLD solver somewhat...

--- source/Riemann_solvers/HLLD_MHD.cpp
+++ source/Riemann_solvers/HLLD_MHD.cpp
@@ -118,7 +118,7 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
       const double gamma,    ///< input gamma
 
       //const pion_flt etamax, ///< H-correction eta-max value.
-      pion_flt *out_p,         ///< output p*
+      //pion_flt *out_p,         ///< output p*
       pion_flt *out_flux       ///< output flux
       )
 {
@@ -126,7 +126,7 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
   //
   // compute conserved U and Flux (m05 eq 3)
   // 
-  double BX = left[eqBX]; // Bx is constant
+  double BX = 0.5*(left[eqBX]+right[eqBX]); // Bx is constant (Should be mean of left and right state)
 
   PtoU(left ,HD_UL,eq_gamma);
   PtoU(right,HD_UR,eq_gamma);
@@ -137,67 +137,58 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
   //
   // compute wave speeds (m05 eq 3)
   // 
-  double 
-    gp_l = gamma * left[eqPG];
-    BB_l = pow(BX,2) + pow(left[eqBY],2)  + pow(left[eqBZ],2);
-    temp = gp_l + BB_l;
-    cf_l = sqrt((temp + sqrt(pow(temp,2) - 4*gp_l * pow(BX,2)))/(2*left[eqRO]));
+  double gp_l = gamma * left[eqPG];
+  double  BB_l = pow(BX,2) + pow(left[eqBY],2)  + pow(left[eqBZ],2);
+  double  temp = gp_l + BB_l;
+  double  cf_l = cfast(left,gamma); //sqrt((temp + sqrt(pow(temp,2) - 4*gp_l * pow(BX,2)))/(2*left[eqRO]));
 
-  double 
-    gp_r = gamma * right[eqPG];
-    BB_r = pow(BX,2) + pow(right[eqBY],2) + pow(right[eqBZ],2);
-    temp = gp_r + BB_r;
-    cf_r = sqrt((temp + sqrt(pow(temp,2) - 4*gp_r * pow(BX,2)))/(2*right[eqRO]));
+  double gp_r = gamma * right[eqPG];
+  double  BB_r = pow(BX,2) + pow(right[eqBY],2) + pow(right[eqBZ],2);
+  temp = gp_r + BB_r;
+  double  cf_r = cfast(right,gamma);  //sqrt((temp + sqrt(pow(temp,2) - 4*gp_r * pow(BX,2)))/(2*right[eqRO]));
 
   double cf_max = max(cf_l,cf_r);
  
   HD_lambda[0] = min(left[eqVX],right[eqVX]) - cf_max; // S_l (m05 eq 67)
-  HD_lambda[4] = max(left[eqVX],right[eqVX]) - cf_max; // S_r
- 
+  HD_lambda[4] = max(left[eqVX],right[eqVX]) + cf_max; // S_r
 
-  double 
-    sl_vl = HD_lambda[0] - left[eqVX];
-    sr_vr = HD_lambda[4] - right[eqVX];
-    tp_r  = right[eqPG]  + 0.5 * BB_r; // total pressure
-    tp_l  = left[eqPG]   + 0.5 * BB_l;
-    temp  = sr_vr  * right[eqRO]  - sl_vl * left[eqRO];
+  double sl_vl = HD_lambda[0] - left[eqVX];
+  double sr_vr = HD_lambda[4] - right[eqVX];
+  double tp_r  = Ptot(right,gamma); //right[eqPG]  + 0.5 * BB_r; // total pressure
+  double tp_l  = Ptot(left, gamma); //left[eqPG]   + 0.5 * BB_l;
+  temp = sr_vr  * right[eqRO]  - sl_vl * left[eqRO];
 
   HD_lambda[2] = (sr_vr * HD_UR[eqMMX] - sl_vl * HD_UL[eqMMX] - tp_r + tp_l)/temp; // S_m (m05 eq 38)
   double tp_s  = (sr_vr * right[eqRO]  * tp_l  - sl_vl * left[eqRO] * tp_r  
 		 + left[eqRO] * right[eqRO] * sr_vr * sl_vl * (right[eqVX] - left[eqVX]))/temp; // total pressure* (m05 eq 41)
- 
-
+  
   //
   // calculate intermediate states
   //
-  double 
-    sl_sm = HD_lambda[0] - HD_lambda[2];
-    sr_sm = HD_lambda[4] - HD_lambda[2];
+  double sl_sm = HD_lambda[0] - HD_lambda[2];
+  double sr_sm = HD_lambda[4] - HD_lambda[2];
 
 
   HD_ULs[eqRHO] = left[eqRO]  * sl_vl/sl_sm; // d* (m05 eq 43)
-  HD_URs[eqRHO] = right[eqRO] * sr_vr/sl_sm;
+  HD_URs[eqRHO] = right[eqRO] * sr_vr/sr_sm;
 
   HD_ULs[eqMMX] = HD_lambda[2] * HD_ULs[eqRHO]; // Vx* (m05 eq 39)
   HD_URs[eqMMX] = HD_lambda[2] * HD_URs[eqRHO];
 
 
-  double 
-    temp_l1 = HD_lambda[2] - left[eqVX];
-    temp_l2 = left[eqRO]   * sl_vl * sl_sm - pow(BX,2);
-    temp_r1 = HD_lambda[2] - right[eqVX];
-    temp_r2 = right[eqRO] * sr_vr * sr_sm - pow(BX,2);
+  double temp_l1 = HD_lambda[2] - left[eqVX];
+  double temp_l2 = left[eqRO]   * sl_vl * sl_sm - pow(BX,2);
+  double temp_r1 = HD_lambda[2] - right[eqVX];
+  double temp_r2 = right[eqRO] * sr_vr * sr_sm - pow(BX,2);
 
-  double 
-    vys_l  = left[eqVY]  - BX * left[eqBY]  * temp_l1/temp_l2; // Vy* (m05 eq. 44)
-    vys_r  = right[eqVY] - BX * right[eqBY] * temp_r1/temp_r2;
+  double vys_l  = left[eqVY]  - BX * left[eqBY]  * temp_l1/temp_l2; // Vy* (m05 eq. 44)
+  double  vys_r  = right[eqVY] - BX * right[eqBY] * temp_r1/temp_r2;
   
   HD_ULs[eqMMY] = vys_l * HD_ULs[eqRHO]; 
   HD_URs[eqMMY] = vys_r * HD_URs[eqRHO]; 
 
-  double 
-    vzs_l  = left[eqVZ]  - BX  * left[eqBZ]  * temp_l1/temp_l2; // Vz* (m05 eq. 46)
-    vzs_r  = right[eqVZ] - BX  * right[eqBZ] * temp_r1/temp_r2;
+  double vzs_l  = left[eqVZ]  - BX  * left[eqBZ]  * temp_l1/temp_l2; // Vz* (m05 eq. 46)
+  double  vzs_r  = right[eqVZ] - BX  * right[eqBZ] * temp_r1/temp_r2;
   
   HD_ULs[eqMMZ] = vzs_l * HD_ULs[eqRHO]; 
   HD_URs[eqMMZ] = vzs_r * HD_URs[eqRHO]; 
@@ -217,26 +208,28 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
 
   temp_l1 = left[eqVX]  * BX + left[eqVY]  * left[eqBY]  + left[eqVZ]  * left[eqBZ]; // B dot v
   temp_r1 = right[eqVX] * BX + right[eqVY] * right[eqBY] + right[eqVZ] * right[eqBZ]; 
-  temp_l2 = HD_lambda[3] * HD_ULs[eqBBX] + vys_l * HD_ULs[eqBBY] + vzs_l * HD_ULs[eqBBZ]; // B* dot v*
-  temp_r2 = HD_lambda[3] * HD_URs[eqBBX] + vys_r * HD_URs[eqBBY] + vzs_r * HD_URs[eqBBZ];
+  temp_l2 = HD_lambda[2] * HD_ULs[eqBBX] + vys_l * HD_ULs[eqBBY] + vzs_l * HD_ULs[eqBBZ]; // B* dot v*
+  temp_r2 = HD_lambda[2] * HD_URs[eqBBX] + vys_r * HD_URs[eqBBY] + vzs_r * HD_URs[eqBBZ];
 
 
   HD_ULs[eqERG] = (sl_vl * HD_UL[eqERG] - tp_l * left[eqVX]  + tp_s * HD_lambda[2] + BX  
 			 * (temp_l1 - temp_l2))/sl_sm;  // e* (m05 eq 48)
   HD_URs[eqERG] = (sr_vr * HD_UR[eqERG] - tp_r * right[eqVX] + tp_s * HD_lambda[2] + BX 
-			 * (temp_r1 - temp_r2))/sl_sm;
-
+			 * (temp_r1 - temp_r2))/sr_sm;
 
   HD_lambda[1] = HD_lambda[2] - abs(BX)/sqrt(HD_ULs[eqRHO]);  // S_l* (m05 eq 51)
   HD_lambda[3] = HD_lambda[2] + abs(BX)/sqrt(HD_URs[eqRHO]);  // S_r*
 
+  //rep.printVec("lambda",HD_lambda,5);
+
 
   // ADD CASE BX=0
   if (BX==0)
-    for (int v=0; v<eq_nvar; v++)
+    for (int v=0; v<eq_nvar; v++) {
       HD_ULss[v] = HD_ULs[v];
       HD_URss[v] = HD_URs[v];
-  else
+    }
+  else {
     HD_ULss[eqRHO] = HD_ULs[eqRHO]; // rho** (m05 eq 49)
     HD_URss[eqRHO] = HD_URs[eqRHO];
 
@@ -266,9 +259,14 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
 
     HD_ULss[eqERG] = HD_ULs[eqERG] - temp_l1 * (temp_l2 - temp) * sgn; // E** (m05 eq 63)
     HD_URss[eqERG] = HD_URs[eqERG] + temp_r1 * (temp_r2 - temp) * sgn;
+  }
 
-
-
+  //rep.printVec("HD_UL",HD_UL,eq_nvar);
+  //rep.printVec("HD_ULs",HD_ULs,eq_nvar);
+  //rep.printVec("HD_ULss",HD_ULss,eq_nvar);
+  //rep.printVec("HD_URss",HD_URss,eq_nvar);
+  //rep.printVec("HD_URs",HD_URs,eq_nvar);
+  //rep.printVec("HD_UR",HD_UR,eq_nvar);
 
   // change out_p with UtoP(HD_UL[v],out_ps,gamma); // Tmin ?
 
@@ -276,29 +274,28 @@ int HLLD_MHD::MHD_HLLD_flux_solver(
   if      (HD_lambda[0]>0)
     for (int v=0; v<eq_nvar; v++) 
       out_flux[v] = HD_FL[v];
-      out_p[v]    = HD_UL[v];
+  //    out_p[v]    = HD_UL[v];
   else if (HD_lambda[1]>=0)
     for (int v=0; v<eq_nvar; v++) // Fl* (m05 eq. 64) 
       out_flux[v] = HD_FL[v] + HD_lambda[0] * (HD_ULs[v] - HD_UL[v]); 
-      out_p[v]    = HD_ULs[v];
+  //    out_p[v]    = HD_ULs[v];
   else if (HD_lambda[2]>=0)
     for (int v=0; v<eq_nvar; v++)   // Fl** (m05 eq. 65)
       out_flux[v] = HD_FL[v] + HD_lambda[1] * HD_ULss[v] - (HD_lambda[1] - HD_lambda[0]) * HD_ULs[v] - HD_lambda[0] * HD_UL[v];
-      out_p[v]    = HD_ULss[v];
+  //    out_p[v]    = HD_ULss[v];
   else if (HD_lambda[3]>=0)
     for (int v=0; v<eq_nvar; v++) 
       out_flux[v] = HD_FR[v] + HD_lambda[3] * HD_URss[v] - (HD_lambda[3] - HD_lambda[4]) * HD_URs[v] - HD_lambda[4] * HD_UR[v];
-      out_p[v]    = HD_URss[v];
+  //    out_p[v]    = HD_URss[v];
   else if (HD_lambda[4]>=0)
     for (int v=0; v<eq_nvar; v++) 
       out_flux[v] = HD_FR[v] + HD_lambda[4] * (HD_URs[v] - HD_UR[v]);
-      out_p[v]    = HD_URs[v];
-  else
-    for (int v=0; v<eq_nvar; v++) 
-      out_flux[v] = HD_FR[v];
-      out_p[v]    = HD_UR[v];
+  //    out_p[v]    = HD_URs[v];
+  else for (int v=0; v<eq_nvar; v++) out_flux[v] = HD_FR[v];
+  //    out_p[v]    = HD_UR[v];
 
-
+  
+  //rep.printVec("out_Flux",out_flux,eq_nvar);
 
 
   return 0;
