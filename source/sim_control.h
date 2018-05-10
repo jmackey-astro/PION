722fb6645bda0092fb78474e1cb4e8cace4a6dc2
Jonathan Mackey <jmackey@cp.dias.ie>
2018-05-10 17:09:38 +0100

    Made calc_timestep into its own class, that inherits from setup_fixed_grid, and is inherited by sim_control.

--- source/sim_control.h
+++ source/sim_control.h
@@ -59,6 +59,7 @@
 #include "decomposition/MCMD_control.h"
 #include "setup_fixed_grid.h"
 #include "sim_control/update_boundaries.h"
+#include "sim_control/calc_timestep.h"
 
 /// The simplest finite volume grid -- a uniform grid with cells that
 /// are cube-shaped in the chosen coordinates.
@@ -66,7 +67,7 @@
 /// This can integrate any system of equations if given the right solver class.
 /// It can solve the equations in 1st or 2nd order accuracy in space and time.
 ///
-class sim_control : virtual public setup_fixed_grid, virtual public update_boundaries
+class sim_control : virtual public setup_fixed_grid, virtual public update_boundaries, virtual public calc_timestep
 {
   public:
   sim_control();  ///< Simple constructor, initialises value.
@@ -134,10 +135,6 @@ class sim_control : virtual public setup_fixed_grid, virtual public update_bound
   //---------------------------------------
   // Class Member data:
   //
-  ///
-  /// information about the simulation
-  ///
-  class SimParams SimPM;
 
   ///
   /// information about multi-core-multi-domain simulations, used for
@@ -172,6 +169,12 @@ class sim_control : virtual public setup_fixed_grid, virtual public update_bound
   /// fluxes on the grid.
   ///
   class FV_solver_base *spatial_solver;
+
+  ///
+  /// information about the simulation
+  ///
+  class SimParams SimPM;
+  
   //---------------------------------------
   //---------------------------------------
 
@@ -215,92 +218,10 @@ class sim_control : virtual public setup_fixed_grid, virtual public update_bound
   ///
   int set_equations();
 
-    
-
-
-   /********************************************************************/
-   // Functions called by time_int()
-
-  // ***********************************************************************
-  // *********************** TIMESTEP CALCULATION **************************
-  // ***********************************************************************
-
-  ///
-  /// Calculate the appropriate timestep.
-  /// 
-  /// For a uniform grid, all cells have the same timestep equal to the minimum
-  /// of all calculated steps.  This function calls two functions, one to get 
-  /// the microphysics timestep (if needed), and another to get the dynamics
-  /// timestep.
-  ///
-  /// \retval 0 success
-  /// \retval 1 failure
-  ///
-  virtual int calc_timestep(
-      class GridBaseClass *, ///< pointer to grid.
-      class RayTracingBase * ///< raytracer for this grid.
-      );
-
-
-  ///
-  /// Calculate the microphysics timestep, based on heating/cooling and reaction
-  /// rates.  Returns the minimum timestep of the local grid (negative if error).
-  /// 
-  double calc_microphysics_dt(
-      class GridBaseClass *, ///< pointer to grid.
-      class RayTracingBase * ///< raytracer for this grid.
-      );
-
-  ///
-  /// Old microphysics timescales calculation with no radiation field.
-  ///
-  double get_mp_timescales_no_radiation(
-      class GridBaseClass * 
-      );
-
-  ///
-  /// New microphysics timescales calculation with pre-calculated radiation field.
-  ///
-  double get_mp_timescales_with_radiation(
-      class GridBaseClass * 
-      );
-
-  ///
-  /// Calculate the dynamics timestep, based on the Courant condition that
-  /// the fastest signals cannot cross a full cell in a single step.  Returns
-  /// the minimum timestep on the local grid, or negative if an error occurs.
-  ///
-  double calc_dynamics_dt(
-      class GridBaseClass * 
-      );
- 
-
-#ifdef THERMAL_CONDUCTION
-  ///
-  /// If doing thermal conduction, this calculates the max. timestep we can use
-  /// without conduction changing the gas temperature by more than 30%.
-  /// It also calls the solver function set_thermal_conduction_Edot() so it 
-  /// knows what the flux in and out of cells is.  This Edot value is multiplied
-  /// by the timestep dt in spatial_solver->preprocess_data().
-  ///
-  double calc_conduction_dt_and_Edot();
-#endif // THERMAL CONDUCTION
-
-  ///
-  /// Limits the timestep based on a number of criteria.  Makes sure we don't
-  /// overshoot the finish-time, or the next output-time, and that we don't 
-  /// increase the timestep by a large factor from one step to the next (this
-  /// can affect stability).
-  ///
-  void timestep_checking_and_limiting();
-
-  // ***********************************************************************
-  // *********************** TIMESTEP CALCULATION **************************
-  // ***********************************************************************
   ///
   /// Advance the simulation by one time step.  This is the main time
-  /// integration function, and calls calc_timestep, followed by either a
-  /// first-order or second-order time update.
+  /// integration function, and calls calculate_timestep, followed by
+  /// either a first-order or second-order time update.
   ///
   /// \retval 0 success
   /// \retval 1 failure
@@ -443,15 +364,6 @@ class sim_control : virtual public setup_fixed_grid, virtual public update_bound
       class GridBaseClass * ///< grid pointer
       );
 
-
-  ///
-  /// Run through all diffuse and direct radiation sources and calculate column
-  /// densities through the grid for each one.  Tau, DTau, and Vshell are stored
-  /// in extra_data[i] for each cell.
-  ///
-  int calculate_raytracing_column_densities(
-      class RayTracingBase * ///< raytracer for this grid.
-      );
 
   ///
   /// Output the data to file if required.
---
+++ source/sim_control/calc_timestep.h
@@ -0,0 +1,148 @@
+/// \file calc_timestep.h
+/// \brief routines for calculating the timestep on a grid.
+/// \author Jonathan Mackey
+/// \date 2018.05.10
+/// 
+/// Description:\n
+/// Has a set of routines for calculating the timestep for fluid
+/// dynamics simulations in PION.
+/// 
+/// Modifications:
+/// - 2018.05.10 JM: moved from sim_control into its own class that
+///    inherits from setup_fixed_grid.
+
+#ifndef CALC_TIMESTEP_H
+#define CALC_TIMESTEP_H
+
+#include "defines/functionality_flags.h"
+#include "defines/testing_flags.h"
+
+#include "sim_params.h"
+#include "grid/grid_base_class.h"
+#include "spatial_solvers/solver_eqn_base.h"
+#include "grid/uniform_grid.h"
+#include "dataIO/dataio_base.h"
+#include "decomposition/MCMD_control.h"
+#include "setup_fixed_grid.h"
+
+///
+/// This class operates on finite-volume computational grid for
+/// fluid dynamics, calculating the allowed timesteps based on
+/// various criteria such as local flow velocities, chemical
+/// timescales, and heat conduction timescales.
+///
+/// It inherits from setup_fixed_grid, and is inherited by
+/// sim_control.
+///
+class calc_timestep : virtual public setup_fixed_grid
+{
+  public:
+
+  calc_timestep();
+  
+  ~calc_timestep();
+
+  ///
+  /// Calculate the appropriate timestep.
+  /// 
+  /// For a uniform grid, all cells have the same timestep equal to the minimum
+  /// of all calculated steps.  This function calls two functions, one to get 
+  /// the microphysics timestep (if needed), and another to get the dynamics
+  /// timestep.
+  ///
+  /// \retval 0 success
+  /// \retval 1 failure
+  ///
+  virtual int calculate_timestep(
+      class SimParams &,      ///< pointer to simulation parameters
+      class GridBaseClass *, ///< pointer to grid.
+      class RayTracingBase *, ///< raytracer for this grid.
+      class FV_solver_base * ///< solver/equations class
+      );
+
+  protected:
+
+  ///
+  /// Calculate the microphysics timestep, based on heating/cooling and reaction
+  /// rates.  Returns the minimum timestep of the local grid (negative if error).
+  /// 
+  double calc_microphysics_dt(
+      class SimParams &,      ///< pointer to simulation parameters
+      class GridBaseClass *, ///< pointer to grid.
+      class RayTracingBase * ///< raytracer for this grid.
+      );
+
+  ///
+  /// Old microphysics timescales calculation with no radiation field.
+  ///
+  double get_mp_timescales_no_radiation(
+      class SimParams &,      ///< pointer to simulation parameters
+      class GridBaseClass * 
+      );
+
+  ///
+  /// New microphysics timescales calculation with pre-calculated radiation field.
+  ///
+  double get_mp_timescales_with_radiation(
+      class SimParams &,      ///< pointer to simulation parameters
+      class GridBaseClass * 
+      );
+
+  ///
+  /// Run through all diffuse and direct radiation sources and calculate column
+  /// densities through the grid for each one.  Tau, DTau, and Vshell are stored
+  /// in extra_data[i] for each cell.
+  ///
+  int calculate_raytracing_column_densities(
+      class SimParams &,      ///< pointer to simulation parameters
+      class RayTracingBase * ///< raytracer for this grid.
+      );
+
+  ///
+  /// Calculate the dynamics timestep, based on the Courant condition that
+  /// the fastest signals cannot cross a full cell in a single step.  Returns
+  /// the minimum timestep on the local grid, or negative if an error occurs.
+  ///
+  double calc_dynamics_dt(
+      class SimParams &,      ///< pointer to simulation parameters
+      class GridBaseClass *, 
+      class FV_solver_base * ///< solver/equations class
+      );
+
+
+#ifdef THERMAL_CONDUCTION
+  ///
+  /// If doing thermal conduction, this calculates the max. timestep we can use
+  /// without conduction changing the gas temperature by more than 30%.
+  /// It also calls the solver function set_thermal_conduction_Edot() so it 
+  /// knows what the flux in and out of cells is.  This Edot value is multiplied
+  /// by the timestep dt in spatial_solver->preprocess_data().
+  ///
+  double calc_conduction_dt_and_Edot(
+      class SimParams &,     ///< pointer to simulation parameters
+      class GridBaseClass *, ///< pointer to grid.
+      class FV_solver_base * ///< solver/equations class
+      );
+#endif // THERMAL CONDUCTION
+
+  ///
+  /// Limits the timestep based on a number of criteria.  Makes sure we don't
+  /// overshoot the finish-time, or the next output-time, and that we don't 
+  /// increase the timestep by a large factor from one step to the next (this
+  /// can affect stability).
+  ///
+  void timestep_checking_and_limiting(
+      class SimParams &      ///< pointer to simulation parameters
+      );
+
+  // ----------------------------------------------------------------
+  // ---- Data Members ----
+  // ----------------------------------------------------------------
+  protected:
+  // ----------------------------------------------------------------
+  // ---- Data Members ----
+  // ----------------------------------------------------------------
+
+};
+
+#endif // CALC_TIMESTEP_H
