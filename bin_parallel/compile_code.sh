0153c6d02d5651d10c1ea66cfdb861a1cd4b5305
Jonathan Mackey <jmackey@cp.dias.ie>
2018-08-28 11:20:01 +0200

    Worked on NG_MPI fine-to-coarse boundaries, and fixed bugs from additions to MCMD class.

--- bin_parallel/compile_code.sh
+++ bin_parallel/compile_code.sh
@@ -242,7 +242,9 @@ echo PION_OPTIONS: $PION_OPTIONS
 #####################################################################
 export MAKE_UNAME
 echo "COMPILING WITH MACHINE: $MAKE_UNAME"
-make -j${NCORES} -f Makefile all
+make -j${NCORES} -f Makefile uni
+make -j${NCORES} -f Makefile NG
+
 #####################################################################
 
 exit
