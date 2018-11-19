4b25615ae2520df1b0db8baa7bd62d008d759a92
Jonathan Mackey <jmackey@cp.dias.ie>
2018-11-19 17:49:11 +0000

    changed calculation of 1st timestep for jet simulation

--- source/sim_control/sim_control_MPI.cpp
+++ source/sim_control/sim_control_MPI.cpp
@@ -391,6 +391,12 @@ int sim_control_pllel::Time_Int(
     err += calculate_timestep(SimPM, grid[0],spatial_solver,0);
     rep.errorTest("TIME_INT::calc_timestep()",0,err);
 
+    if (SimPM.timestep==0 && JP.jetic!=0) {
+      //cout <<SimPM.dt <<", "<<0.1*SimPM.CFL*grid[0]->DX()/JP.jetstate[VX]<<"\n";
+      SimPM.dt = std::min(SimPM.dt, 0.1*SimPM.CFL*grid[0]->DX()/JP.jetstate[VX]);
+      spatial_solver->Setdt(SimPM.dt);
+    }
+
 #ifdef TESTING
     cout <<"MPI time_int: stepping forward in time\n";
 #endif
