37089428189f6bcff9e4cccbe2f667856ba4710b
Jonathan Mackey <jmackey@cp.dias.ie>
2020-03-26 18:24:04 +0100

    edited advection test

--- test_problems/advection/plot_final_state.py
+++ test_problems/advection/plot_final_state.py
@@ -10,12 +10,18 @@ import Silo
 sys.path.append("/home/jm/code/pypion/Library")
 import Plotting_Classes as ppion
 
-for file in ["advection_v020_t30_level00_0000.00000000.silo","advection_v020_t30_level00_0000.00007394.silo", "adv_l1_0000.00003697.silo"]:
+#for file in ["advection_v020_t30_level00_0000.00000000.silo","advection_v020_t30_level00_0000.00007394.silo", "adv_l1_0000.00003697.silo"]:
+for file in ["advection_v020_t30_l1n128_0000.00001847.silo", 
+             "advection_v020_t30_l1n256_0000.00003694.silo", 
+             "advection_v020_t30_l1n512_0000.00007388.silo", 
+             "advection_v020_t30_l2n128_level00_0000.00003700.silo",
+             "advection_v020_t30_l2n256_level00_0000.00007394.silo"]:
   plt.figure()
   plotting=ppion.Plotting2d(file)
   n = plotting.nlevels()
   c = plotting.cycle()
   t = plotting.sim_time().value
+  level_ng  = plotting.ngrid()
 
   for ilev in range(n):
     #if ilev==0 and n>1:
@@ -35,7 +41,6 @@ for file in ["advection_v020_t30_level00_0000.00000000.silo","advection_v020_t30
     d = plotting.reshaped_parameter2d("Density")
     level_min = plotting.level_min()
     level_max = plotting.level_max()
-    level_ng  = plotting.ngrid()
     dy=((level_max[1]-level_min[1])).value/level_ng[1]
     dx=((level_max[0]-level_min[0])).value/level_ng[0]
     y, x = np.mgrid[slice(level_min[1].value, level_max[1].value, dy),
@@ -79,6 +84,6 @@ for file in ["advection_v020_t30_level00_0000.00000000.silo","advection_v020_t30
   st = "Density, t="+tm
   plt.text(0.51,1.43,st,color="white")
 #plt.show()
-  fn = "advection2D_l"+str(n)+"c"+str(c)+".png"
+  fn = "advection2D_l"+str(n)+"n"+str(level_ng[0])+"c"+str(c)+".png"
   plt.savefig(fn,bbox_inches="tight",dpi=300)
 quit()
