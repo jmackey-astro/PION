72eb924bfa91395b39ec3b3d7d9bbe65bdc5e243
Jonathan Mackey <jmackey@cp.dias.ie>
2021-02-04 18:27:10 +0100

    changed cvode include

--- source/microphysics/MPv2.h
+++ source/microphysics/MPv2.h
@@ -225,9 +225,9 @@ extern class mp_rates_ExpH_ImpMetals MPR;
 //
 // Header files for CVodes solver.
 //
-#include <cvodes/cvodes.h>           // prototypes for CVODE fcts. and consts.
+#include <cvode/cvode.h>           // prototypes for CVODE fcts. and consts.
 #include <nvector/nvector_serial.h>  // serial N_Vector types, fcts., and macros
-#include <cvodes/cvodes_dense.h>     // prototype for CVDense
+#include <cvode/cvode_dense.h>     // prototype for CVDense
 #include <sundials/sundials_dense.h> // definitions CVMatrix DENSE_ELEM
 #include <sundials/sundials_types.h> // definition of type realtype
 #include <cstdio>
